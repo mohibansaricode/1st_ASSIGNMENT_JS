89b3d868c2e966faec1de91111f846e17fe02b33
mohibansaricode <mmcode46@gmail.com>
2025-05-09 23:27:58 +0500

    1st question in JS

---
+++ app.js
@@ -0,0 +1,10 @@
+// QUESTION NO 1: Create two integer variables length and breadth, assign values, then check if they are square values or rectangle values. If both values are equal, it’s a square; otherwise, it’s a rectangle.
+
+let length = 9;
+let breadth = 10;
+
+if (length === breadth) {
+    console.log("Its a Square");
+}else{
+    console.log("Its a rectangle");
+}

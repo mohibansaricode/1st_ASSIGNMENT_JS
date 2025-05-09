761d913f83b6b610d485488df67e44e911815c08
mohibansaricode <mmcode46@gmail.com>
2025-05-09 23:35:20 +0500

    2nd question in JS

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 // QUESTION NO 1: Create two integer variables length and breadth, assign values, then check if they are square values or rectangle values. If both values are equal, it’s a square; otherwise, it’s a rectangle.
 
-let length = 9;
-let breadth = 10;
+// let length = 9;
+// let breadth = 10;
 
-if (length === breadth) {
-    console.log("Its a Square");
-}else{
-    console.log("Its a rectangle");
-}
+// if (length === breadth) {
+//     console.log("Its a Square");
+// }else{
+//     console.log("Its a rectangle");
+// }
+
+// QUESTION NO 2: Take two variables storing ages, then use if/else to determine the oldest and youngest among them.
+
+// let userOne = 30;
+// let userTwo = 30;
+
+// if (userOne > userTwo) {
+//     console.log("user one is oldest");
+// }else if(userTwo > userOne){
+//     console.log("user Two is oldest");
+// }else{
+//     console.log("Both are equal");
+// }
+
